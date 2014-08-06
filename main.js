2bc4aa2420912c1bf7de1d5c50de0dd147862383
James H. Edwards <james.howe.edwards@gmail.com>
2014-08-06 09:39:43 -0700

    moved source files into src/ directory, created main.js to hold module.exports and package.json for npm information

---
+++ main.js
@@ -0,0 +1,23 @@
+var heapSort = require('./src/heapSort');
+var mergeSort = require('./src/mergeSort');
+var insertionSort = require('./src/insertionSort');
+var sleepSort = require('./src/sleepSort');
+var radixSort = require('./src/radixSort');
+var bogoSort = require('./src/bogoSort');
+var cocktailSort = require('./src/cocktailSort');
+var quickSort = require('./src/quickSort');
+var bubbleSort = require('./src/bubbleSort');
+
+module.exports = {
+
+heapSort  : heapSort,
+mergeSort : mergeSort,
+insertionSort : insertionSort,
+sleepSort : sleepSort,
+radixSort : radixSort,
+bogoSort  : bogoSort,
+cocktailSort : cocktailSort,
+quickSort : quickSort,
+bubbleSort: bubbleSort
+
+}
